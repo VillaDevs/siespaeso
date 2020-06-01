6f761446dc9359994e195b9c8a17bbe7536e13de
Audel Diaz <audel.diaz@outlook.com>
2020-05-31 19:03:03 -0500

    base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCgXjAWRKvxQaItiItpcXZbyGIxYJRb77I",
+    authDomain: "siespaeso.firebaseapp.com",
+    databaseURL: "https://siespaeso.firebaseio.com",
+    projectId: "siespaeso",
+    storageBucket: "siespaeso.appspot.com",
+    messagingSenderId: "1072245394363",
+    appId: "1:1072245394363:web:1c794ad683f204c180529e",
+    measurementId: "G-ZPKM54L25D"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCgXjAWRKvxQaItiItpcXZbyGIxYJRb77I",
+    authDomain: "siespaeso.firebaseapp.com",
+    databaseURL: "https://siespaeso.firebaseio.com",
+    projectId: "siespaeso",
+    storageBucket: "siespaeso.appspot.com",
+    messagingSenderId: "1072245394363",
+    appId: "1:1072245394363:web:1c794ad683f204c180529e",
+    measurementId: "G-ZPKM54L25D"
+  }
 };
 
 /*
