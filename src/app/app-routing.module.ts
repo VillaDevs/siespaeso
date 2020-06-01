b3e01d8a664b70de48155593b86c2259aa80370c
Jose Alvarez <alvarej2@colpatria.com>
2020-05-31 20:29:37 -0500

    ♻️Added business module and removed unnecessary code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'business',
+    loadChildren: () => import('./business/business.module').then( m => m.BusinessPageModule)
   }
 ];
 
@@ -19,4 +18,5 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {}
